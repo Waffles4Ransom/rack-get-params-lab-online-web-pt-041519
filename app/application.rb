548d475ee924e12a4e4cd3a50980fd3e5c8e7c00
Jenna Ransom <jenna.ransom@gmail.com>
2019-07-20 04:56:08 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -26,6 +26,10 @@ class Application
       add_item = req.params["item"]
       if @@items.include?(add_item)
         @@cart << add_item
+        resp.write "added #{add_item}"
+      else 
+        resp.write "We don't have this item"
+      end 
     else
       resp.write "Path Not Found"
     end
